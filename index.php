f7e5bce2af1acf3c7e18c22d1c79cfb0d90cca45
Jhon Hader Rodriguez Perdomo <jhonhader2@hotmail.com>
2025-04-07 10:24:41 -0500

    primer commit

---
+++ index.php
@@ -0,0 +1,274 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./assets/css/style.css">
+    <link rel="stylesheet" href="./assets/css/header-logo.css">
+    <link rel="stylesheet" href="../assets/css/section.css">
+    <link rel="stylesheet" href="../assets/css/section_1.css">
+    <link rel="stylesheet" href="../assets/css/section_2.css">
+    <link rel="stylesheet" href="../assets/css/section_3.css">
+    <link rel="stylesheet" href="../assets/css/section_4.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+    <!-- inicio container principal -->
+    <div class="container-fluid">
+        <!-- inicion header -->
+        <header class="row mt-5">
+            <div class="col-md-12 justify-content-center header-logo">
+            </div>
+        </header>
+        <div class="row mt-5">
+            <div class="col-md-6 d-flex justify-content-center">
+                <img src="./assets/images/logo-sena-3.png" alt="">
+            </div>
+            <div class="col-md-6">
+                <div class="header-title">
+                    <h1>Tecnología en Gestión Integral de la Biodiversidad y los Servicios Ecosistémicos</h1>
+                    <p>Programa de Educación Superior SENA</p>
+                </div>
+            </div>
+        </div>
+        <!-- fin header -->
+        <!-- inicio section -->
+        <div class="row description mt-5 image-description">
+            <div class="col-md-6 d-flex justify-content-center">
+                <div class="card card-info mt-3 " style="opacity: 0.5; height: 450px">
+                    <div class="card-header">
+                        <h5 class="text-center">Información del programa</h5>
+                    </div>
+                    <div class="card-body " style="height: 400px;">
+                        <ul>
+                            <li>Nivel de la formación: Tecnologo</li>
+                            <li>Modalidad: Presencial</li>
+                            <li>Nombre del Programa: Gestion integral de la biodiversidad y los servicios
+                                ecosistémicos</li>
+                            <li>ID del programa: 222212</li>
+                            <li>Periodicidad: Trimestral, de acuerdo con la oferta de cada centro de formación SENA</li>
+                            <li>Servicio Nacional de Aprendizaje SENA</li>
+                        </ul>
+                            <a class="btn btn-success" href="">
+                                consulta aqui
+                            </a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 section-text">
+                <div class="card card-info card-info-2 mt-3 " style="opacity: 0.5; height: 450px">
+                    <div class="card-header">
+                        <h5 class="text-center">¿que se estudia en el programa?</h5>
+                    </div>
+                    <div class="card-body " style="height: 400px;">
+                       <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta quia voluptatem velit expedita! Impedit vel voluptatibus maiores dolorum doloribus alias, magni ut et natus libero sapiente iste, culpa mollitia veniam.
+
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta quia voluptatem velit expedita! Impedit vel voluptatibus maiores dolorum doloribus alias, magni ut et natus libero sapiente iste, culpa mollitia veniam.
+                       </p>
+                        <a class="btn btn-success" href="">
+                            consulta aqui
+                        </a>
+                    
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- fin section -->
+
+        <!-- inicio section 2 -->
+        <div class="row no-gutters mt-5">
+            <div class="col-md-12 video-container">
+                <video autoplay muted loop class="background-video">
+                    <source src="./assets/videos/Steven-video.mp4" type="video/mp4">
+                </video>
+                    <div class="video-gradient">
+                        <div class="col-md-6 justify-content-center video-section p-2" >
+                            <h3 class="text-center mt-5">¿Qué es la gestion integral de la biodiversidad?</h3>
+                                <p>
+                                    Entendemos la biodiversidad como la riqueza y la gran variedad de formas de vida
+                                    presentes en los ecosistemas de nuestro municipio, departamento, país y planeta. Aqui,
+                                    todos los organismos hacen parte de una intrincadísima red de vida en donde cada especie
+                                    juega un papel importante en el equilibro de todos los ecosistemas.
+
+                                    Colombia es una potencia mundial de la biodiversidad, una “Potencia mundial de la
+                                    vida”. La gestión de todo ese magnifico potencial de biodiversidad es un desafió complejo
+                                    que requiere personal capacitado y entrenado en las diversas estrategias diseñadas para
+                                    gestionar la biodiversidad, y que se pueden agrupar en tres categorías principalmente:
+
+                                </p>
+                                <ul class="list_1">
+                                    <li> Conservación </li>
+                                    <li> Uso sostenible de los recursos </li>
+                                    <li> Gobernanza y empoderamiento local </li>
+                                </ul>
+                        </div> 
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- fin section 2 -->
+
+        <div class="row justify-content-center">
+        <div class="col-md-12 justify-content-center places">
+                <div class="card card-data" style="height: 400px; opacity: 0.5">
+                    <div class="card-header">
+                        <h4 class="text-center">¿En dónde se puede estudiar este programa?</h4>
+                    </div>
+                    <div class="card-body">
+                        <div class="row card-place">
+                        <div class="col-md-4">
+                        <ul>
+                                APARTADÓ, ANTIOQUIA
+                                ARMENIA, QUINDÍO
+                                BARRANCABERMEJA, SANTANDER
+                                BUGA, VALLE DEL CAUCA
+                                CALDAS, ANTIOQUIA
+                                CAMPOALEGRE, HUILA
+                                CARTAGENA, BOLÍVAR
+                                CAUCASIA, ANTIOQUIA
+                                CHÍA, CUNDINAMARCA
+                                CÚCUTA, NORTE DE SANTANDER
+                                DUITAMA, BOYACÁ
+                                EL BAGRE, ANTIOQUIA
+                                ESPINAL, TOLIMA
+                                FUSAGASUGÁ, CUNDINAMARCA
+                                GARZÓN, HUILA
+                                GRANADA, META
+                            </ul>
+                        </div>
+                        <div class="col-md-4">
+                            <ul>
+                                INÍRIDA, GUAINÍA
+                                LA PLATA, HUILA
+                                LETICIA, AMAZONAS
+                                MANIZALES, CALDAS
+                                MITÚ, VAUPÉS
+                                MOCOA, PUTUMAYO
+                                MONTERÍA, CÓRDOBA
+                                MOSQUERA, CUNDINAMARCA
+                                PASTO, NARIÑO
+                                PEREIRA, RISARALDA
+                                PIEDECUESTA, SANTANDER
+                                PITALITO, HUILA
+                                POPAYÁN, CAUCA
+                                PUERTO ASIS, PUTUMAYO
+                                PUERTO BERRIO, ANTIOQUIA
+                                
+                                
+                                
+                            </ul>
+                        </div>
+                        <div class="col-md-4">
+                            <ul>
+                                PUERTO CARREÑO, VICHADA
+                                RIONEGRO, ANTIOQUIA
+                                SABANALARGA, ATLÁNTICO
+                                SAN ANDRES, ARCHIPIÉLAGO DE SAN ANDRÉS
+                                SAN JOSÉ DEL GUAVIARE, GUAVIARE
+                                SANTAFÉ DE ANTIOQUIA, ANTIOQUIA
+                                TULUÁ, VALLE DEL CAUCA
+                                VALLEDUPAR, CESAR
+                                VILLAVICENCIO, META
+                                VILLETA, CUNDINAMARCA
+                                YOPAL, CASANARE
+                            </ul>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- section 3 -->
+        <div class="row section_3_final">
+            <div class="col-md-12 d-flex justify-content-center">  
+               <div class="acordeon">
+                    <h4 class="text-center">estructura curricular</h4>
+                    <div class="accordion-item">
+                        <div class="accordion-header">
+                            <h5>Competencia 1</h5>
+                        </div>
+                        <div class="accordion-body">
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut molestias excepturi ex pariatur, illum quis iste deserunt quisquam cum cumque atque, itaque animi, odit iure nostrum aliquid ducimus adipisci saepe.</p>
+                        </div>
+                    </div>
+                    <div class="accordion-item">
+                        <div class="accordion-header">
+                            <h5>Competencia 2</h5>
+                        </div>
+                        <div class="accordion-body">
+                            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Necessitatibus enim eos, atque, quis modi perferendis eaque, voluptas officiis quidem suscipit hic. Quo itaque magnam placeat nesciunt hic ducimus eius veniam!</p>
+                        </div>
+                    </div>
+                    <div class="accordion-item">
+                        <div class="accordion-header">
+                            <h5>Competencia 3</h5>
+                        </div>
+                        <div class="accordion-body">
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rem culpa velit quae non. Reiciendis placeat quaerat tenetur esse quibusdam. Temporibus quo at excepturi, aliquid consectetur adipisci repellat tempore veniam ad!</p>
+                        </div>
+                    </div>
+                    <div class="accordion-item">
+                        <div class="accordion-header">
+                            <h5>Competencia 4</h5>
+                        </div>
+                        <div class="accordion-body">
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi facere perferendis excepturi voluptatibus. Illo, praesentium recusandae. Placeat dolorum, ducimus, facere a voluptas, natus reiciendis magnam tempore recusandae quidem voluptatem ipsam?</p>
+                        </div>
+                    </div>
+                    <div class="accordion-item">
+                        <div class="accordion-header">
+                            <h5>Competencia 5</h5>
+                        </div>
+                        <div class="accordion-body">
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi facere perferendis excepturi voluptatibus. Illo, praesentium recusandae. Placeat dolorum, ducimus, facere a voluptas, natus reiciendis magnam tempore recusandae quidem voluptatem ipsam?</p>
+                        </div>
+                    </div>
+                    <div class="accordion-item">
+                        <div class="accordion-header">
+                            <h5>Competencia 6</h5>
+                        </div>
+                        <div class="accordion-body">
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi facere perferendis excepturi voluptatibus. Illo, praesentium recusandae. Placeat dolorum, ducimus, facere a voluptas, natus reiciendis magnam tempore recusandae quidem voluptatem ipsam?</p>
+                        </div>
+                    </div>
+               </div>
+            </div>
+        </div>
+        <!-- fin section 3 --> 
+         <div class="row">
+            <div class="col-md-12 justify-content-center contact-data">
+                <h1 class="text-center">Contacto a nivel nacional</h1>
+                    <div class="info-container">
+                        <p class="text_contact">
+                            Póngase en contacto con nosotros
+                            Contacto a nivel nacional.
+                            <br>
+                            Servicio Nacional de Aprendizaje SENA
+
+                            Dirección general. Calle 57 No. 8 - 69 Bogotá D.C. (Cundinamarca), Colombia
+
+                            El SENA brinda a la ciudadanía, atención presencial en las 33 Regionales y 117 Centros de
+                            Formación Conozca aquí los puntos de atención
+
+                            Líneas de atención al ciudadanos, empresarios.
+                            <ul class="info">
+                                <li>línea PQRS: Bogotá +(57) 601 736 60 60</li>
+                                <li>Línea gratuita y resto del país 018000 910270 </li>
+                                <li>
+                                    <a href="https://www.sena.edu.co/es-co/Paginas/default.aspx" target="_blank">www.sena.edu.co</a>
+                                </li>
+                            </ul>
+                        </p>
+                    </div>
+            </div>
+         </div>
+    </div>
+    <!-- fin container principal -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+    
+</body>
+</html>
