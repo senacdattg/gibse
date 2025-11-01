83da41b558c6bc887a97769a9dcdba730e138435
Jhon Hader Rodriguez <jhonhader2@hotmail.com>
2025-11-01 13:21:40 -0500

    Fix Responsive Mobile

--- index.php
+++ index.php
@@ -12,6 +12,7 @@
     <link rel="stylesheet" href="assets/css/section_2.css">
     <link rel="stylesheet" href="assets/css/section_3.css">
     <link rel="stylesheet" href="assets/css/section_4.css">
+    <link rel="stylesheet" href="assets/css/responsive.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
